e15fe82fea7e0dd31886bab3244458b57b477858
User <dismitha12345678@gmail.com>
2025-05-07 01:52:10 +0530

    api

--- Frontend/src/components/Utils/api.tsx
+++ Frontend/src/components/Utils/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Item } from '../Data/ProductType';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://chip-heaven-react-fullstack-2.onrender.com';
 
 export const fetchItems = async (): Promise<Item[]> => {
     const response = await axios.get<Item[]>(`${API_BASE_URL}/items`);
